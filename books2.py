f566dab671286a5c8d0c2f2139ce76659b5de7e6
Carlos Filho <carlosalmeidafilho96@gmail.com>
2025-02-19 00:59:57 -0300

    Update books2.py

--- books2.py
+++ books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 from starlette import status
 
@@ -59,6 +59,7 @@ async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail="Item not found")
 
 @app.get("/books/", status_code=status.HTTP_200_OK)            
 async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
@@ -87,13 +88,21 @@ def find_book_id(book: Book):
 
 @app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")
 
 @app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Item not found")        
